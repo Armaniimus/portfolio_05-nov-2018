6342431d0b4ce3735e610d101bfda987c5c38e87
Armand_laptop <517226@edu.rocmn.nl>
2018-11-10 15:07:54 +0100

    rewrote config and htacces to match the shorter foldername

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-Define("MAIN_SITE", "http://localhost/ontwikkelomgeving_portfolio/portfolio");
-Define("PROJECTEN_MAIN_URL", "http://localhost/ontwikkelomgeving_portfolio/projectFiles/projecten/");
+Define("MAIN_SITE", "/ontwik/portfolio");
+Define("PROJECTEN_MAIN_URL", "/ontwik/projectFiles/projecten/");
 
 Define("BESTAND_DIEPTE", 2);
 
