7ee345d352853bacef27c68d188a9b3132606a33
Armand_laptop <517226@edu.rocmn.nl>
2018-11-11 15:12:46 +0100

    updated page for a more mobile friendly grid

--- portfolio/wp-content/themes/Armaniimus-portfolio/page.php
+++ portfolio/wp-content/themes/Armaniimus-portfolio/page.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
     <main class="row">
-        <div class="float-l col-xs-1"><br></div>
-        <div class="float-l col-xs-10 container">
+        <div class="col-xs-0 col-s-1" ><br></div>
+        <div class="col-xs-12 col-s-10 container">
 
             <!-- Repeating posts -->
             <div class="row">
-                <div class="float-l col-xs-12 col-xl-6 small-container">
+                <div class="col-xs-12 col-xl-6 small-container">
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
                         <section class="post" id="<?php the_title(); ?>">
@@ -24,7 +24,7 @@
             </div>
 
         </div>
-        <div class="float-l col-xs-1"><br></div>
+        <div class="col-xs-0 col-s-1" ><br></div>
     </main>
     <!-- <h1>index file </h1> -->
 <?php get_footer(); ?>
