7327cd2e5de8e16d604ce8eceee09aa4b8e000b7
Armand_laptop <517226@edu.rocmn.nl>
2018-11-11 15:14:21 +0100

    compacted 3 files together in index.php

--- portfolio/wp-content/themes/Armaniimus-portfolio/index.php
+++ portfolio/wp-content/themes/Armaniimus-portfolio/index.php
@@ -6,14 +6,46 @@
             <!-- TitleHead -->
             <div class="row title-wrap">
                 <div class="title-head float-l col-xs-12 col-m-12">
-                    <?php include "posts1.php"; ?>
+
+                    <?php
+                    $armaniimus_ii = 0;
+                    if ( have_posts() ) : while ( have_posts() ) : the_post();
+                        $armaniimus_ii++;
+                    endwhile; endif;?>
+
+                    <?php if ( $armaniimus_ii > 1 ) {
+                        echo "<h2>Artikelen op deze pagina</h2>";
+                        echo "<ul class='post-title-ul'>";
+                    } ?>
+
+                    <?php if ( $armaniimus_ii > 1 ) : while ( have_posts() ) : the_post(); ?>
+                        <li class="post-title-li">
+                            <a class="post-title-a" href="#<?php the_title() ?>"><?php the_title() ?></a>
+                        </li>
+                    <?php endwhile; endif; ?>
+
+                    <?php if ( $armaniimus_ii > 1 ) {echo "</ul>";} ?>
+
                 </div>
             </div> <!-- end of TitleHead-->
 
             <!-- Repeating posts -->
             <div class="row">
                 <div class="float-l col-xs-12 col-xl-6 small-container">
-                    <?php include "posts2.php"; ?>
+
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                        <section class="post" id="<?php the_title(); ?>">
+                            <h3 class="post_title"><?php the_title(); ?></h3>
+                            <?php the_content(); ?>
+                        </section>
+
+                    <?php endwhile; else : ?>
+
+                        <?php _e('Sorry, we hebben uw gevraagde content niet kunnen vinden.'); ?>
+
+                    <?php endif; ?>
+
                 </div>
             </div>
 
