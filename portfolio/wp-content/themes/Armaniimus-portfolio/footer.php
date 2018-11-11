cc0d6bb5bb99b222ceed20be43e585425da63c8d
Armand_laptop <517226@edu.rocmn.nl>
2018-11-11 12:59:52 +0100

    footer is now editable through widgets * details in description
    * added widget support in the theme.
    * added 3 widget areas in the footer.
    * changed styling to use the widget areas

--- portfolio/wp-content/themes/Armaniimus-portfolio/footer.php
+++ portfolio/wp-content/themes/Armaniimus-portfolio/footer.php
@@ -3,45 +3,33 @@
 
         <!-- collumn-1 -->
         <ul class="float-l col-xs-4 float footer-box">
-            <li>
-                <h5 class="footer-h5">Onze visie</h5>
-                <p class="footer-visie">
-                    Wij van ArmaniimusWebdevelopment geloven dat technologie niet op zichzelf staat
-                </p>
-                <p class="footer-visie">
-                    Het moet gebracht worden met vriendelijke hand.
-                </p>
-                <p class="footer-visie">
-                    met aandacht voor de eind gebruiker voor een maximum resultaat.
-                </p>
-                <p class="footer-visie footer-visie-bottom">
-                    Koude technologie met een vriendelijk gezicht.
-                </p>
-            </li>
+            <?php if( !dynamic_sidebar( 'foot1' ) ): ?>
+
+        		<h5 class="module-heading">footer1</h5>
+        		<p>Please add widgets via the admin area!</p>
+
+        	<?php endif; ?>
         </ul>
 
         <!-- collumn-2 -->
         <ul class="float-l col-xs-4 float footer-box">
-            <li>
-                <h5 class="footer-h5">Meest interesante pagina's</h5>
-                <ul class="footer-middle-box footer-linkbox">
-                    <li class="footer-link"><a href="overmij">Over mij</a></li>
-                    <li class="footer-link"><a href="projecten-checkers">Draughts Project</a></li>
-                    <li class="footer-link"><a href="projecten-metvid">Projecten met video</a></li>
-                    <li class="footer-link"><a href="projecten-zondervid">Projecten zonder video</a></li>
-                </ul>
-            </li>
+            <?php if( !dynamic_sidebar( 'foot2' ) ): ?>
+
+        		<h5 class="module-heading">footer2</h5>
+        		<p>Please add widgets via the admin area!</p>
+
+        	<?php endif; ?>
         </ul>
 
         <!-- collumn-3 -->
+
         <ul class="float-l col-xs-4 float footer-box">
-            <li>
-                <h5 class="footer-h5">Contact etc.</h5>
-                <ul class="footer-linkbox">
-                    <li class="footer-link"><a href="contact">Contact</a></li>
-                    <li class="footer-link"><a href="sitemap">Sitemap</a></li>
-                </ul>
-            </li>
+            <?php if( !dynamic_sidebar( 'foot3' ) ): ?>
+
+        		<h5 class="module-heading">footer3</h5>
+        		<p>Please add widgets via the admin area!</p>
+
+        	<?php endif; ?>
         </ul>
     </ul>
 
--- portfolio/wp-content/themes/Armaniimus-portfolio/functions.php
+++ portfolio/wp-content/themes/Armaniimus-portfolio/functions.php
@@ -1,7 +1,9 @@
 <?php
+// theme support
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
 
+// theme menus
 function register_theme_menus() {
     register_nav_menus(
         array (
@@ -11,11 +13,32 @@ function register_theme_menus() {
 }
 add_action('init', 'register_theme_menus');
 
+// widgets
+function armaniimus_create_widget( $name, $id, $description ) {
+
+	register_sidebar(array(
+		'name' => __( $name ),
+		'id' => $id,
+		'description' => __( $description ),
+		'before_widget' => "<div class='widget-foot' id='$id'>",
+		'after_widget' => '</div>',
+		'before_title' => '<h5 class="widget-foot-title">',
+		'after_title' => '</h5>'
+	));
+}
+armaniimus_create_widget( 'footer1', 'foot1', 'the first footerblok' );
+armaniimus_create_widget( 'footer2', 'foot2', 'the second footerblok' );
+armaniimus_create_widget( 'footer3', 'foot3', 'the third footerblok' );
+
+
+// theme css
 function armaniimus_theme_styles() {
     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/css/main.css');
 }
 add_action( 'wp_enqueue_scripts', 'armaniimus_theme_styles');
 
+
+// theme js
 function armaniimus_theme_js() {
     wp_enqueue_script('hamburger_controller_js', get_template_directory_uri() . '/js/hamburger_controller.js', '', '', true );
     wp_enqueue_script('nav_controller_js', get_template_directory_uri() . '/js/nav_controller.js', '', '', true );
